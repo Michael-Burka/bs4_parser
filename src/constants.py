c0509cd16b47a5d952af71c73460d0de434758db
Michael-Burka <contact@michaelburka.com>
2024-03-05 16:43:45 +0400

    Update lxml version and add core project files
    - Updated lxml version in requirements.txt for enhanced parsing capabilities.
    - Added src/configs.py for project configuration management.
    - Introduced src/constants.py for defining constant values used across the project.
    - Created src/exceptions.py to handle custom exception classes.
    - Implemented the main logic in src/main.py.
    - Added src/outputs.py for managing output formatting and storage.
    - Generated a new results file with latest version data: src/results/latest-versions_2024-03-05_16-23-29.csv.
    - Developed src/utils.py for utility functions that support parsing and data manipulation.

---
+++ src/constants.py
@@ -0,0 +1,16 @@
+# constants.py
+from pathlib import Path
+
+MAIN_DOC_URL = 'https://docs.python.org/3/'
+PEP_DOC_URL = 'https://peps.python.org/'
+BASE_DIR = Path(__file__).parent
+EXPECTED_STATUS = {
+    'A': ('Active', 'Accepted'),
+    'D': ('Deferred',),
+    'F': ('Final',),
+    'P': ('Provisional',),
+    'R': ('Rejected',),
+    'S': ('Superseded',),
+    'W': ('Withdrawn',),
+    '': ('Draft', 'Active'),
+}
